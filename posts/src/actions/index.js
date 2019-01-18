411a27e2ab5efb83a57fb70b65f924451d97ec73
Prateek Madaan <prince.cenation@gmail.com>
2019-01-18 18:15:47 +0530

    demo on memoization using lodash to save unnecessary network requests

--- posts/src/actions/index.js
+++ posts/src/actions/index.js
@@ -1,4 +1,7 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+//use lodash for the memoization
+import * as _ from "lodash";
+
 // Request and dispatching an action will go inside the action creator
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get("/posts");
@@ -8,11 +11,16 @@ export const fetchPosts = () => async dispatch => {
   });
 };
 
-export const fetchUser = userId => async dispatch => {
-    const response = await jsonPlaceholder.get(`/users/${userId}`);
-    // console.log(response);
-    dispatch({
-        type : 'FETCH_USER',
-        payload : response.data 
-    })
+export const fetchUser = userId => dispatch => {
+  _fetchUser(userId, dispatch);
 };
+
+//private function
+const _fetchUser = _.memoize(async (userId, dispatch) => {
+  const response = await jsonPlaceholder.get(`/users/${userId}`);
+  // console.log(response);
+  dispatch({
+    type: "FETCH_USER",
+    payload: response.data
+  });
+});
